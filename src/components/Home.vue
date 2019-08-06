0699464235e4cc38b0a415f65281f720679937bb
weixin4036 <408516111@qq.com>
2019-08-06 13:33:49 +0800

    Initial commit

---
+++ src/components/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <node-header></node-header>
+  </div>
+</template>
+
+<script>
+import nodeHeader from './header.vue'
+export default {
+  name: 'home',
+  data () {
+    return {
+      msg: 'Hello world'
+    }
+  },
+  components:{nodeHeader},
+  methods: {
+
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ src/components/header.vue
@@ -0,0 +1,30 @@
+<template>
+	<div>
+		<ul>
+			<li>这是logo</li>
+			<li><i>搜索</i><input type="text"></li>
+		</ul>
+		<ul>
+			<li><router-link :to="{name:'home'}">首页</router-link></li>
+			<li><router-link :to="{name:'home'}">新手入门</router-link></li>
+			<li><router-link :to="{name:'home'}">API</router-link></li>
+			<li><router-link :to="{name:'home'}">关于</router-link></li>
+			<li><router-link :to="{name:'home'}">注册</router-link></li>
+			<li><router-link :to="{name:'home'}">登录</router-link></li>
+		</ul>
+	</div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+<style scoped>
+	
+</style>
